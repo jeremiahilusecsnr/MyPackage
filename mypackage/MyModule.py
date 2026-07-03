8466784bb1a7859d78a15d3eaac1ccb8c4fed9db
jeremiahilusecsnr <jeremiahilusecsnr@gmail.com>
2026-07-03 15:33:15 +0100

    First commit

--- mypackage/MyModule.py
+++ mypackage/MyModule.py
@@ -1,4 +1,19 @@
 def top_n (items, n):
+        """Return the top n items in an array, in descending order.
+
+    Args:
+        items (array): list or array-like object containing numerical values.
+        n (int): number of top items to return.
+
+    Returns:
+        array: top n items, in descending order.
+
+    Examples:
+        >>> top_n([8, 3, 2, 7, 4], 3)
+        [8, 7, 4]
+    """
+
+    # We add the body of the function just below the docstring:
     for i in range(n):
         for j in range(len(items)-1-i):
             if items[j] > items[j+1]:
@@ -6,3 +21,35 @@ def top_n (items, n):
         
     top_n = items[-n:]
     return top_n[::-1]
+
+
+def fibonacci(n):
+
+    """
+    Calculate nth term in fibonacci sequence
+    
+    Args:
+        n (int): nth term in fibonacci sequence to calculate
+    
+    Returns:
+        int: nth term of fibonacci sequence,
+             equal to sum of previous two terms
+    
+    Examples:
+        >>> fibonacci(1)
+        1        
+        >> fibonacci(2)
+        1
+        >> fibonacci(3)
+        2
+    """
+
+    if n <= 1:
+        return n
+
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+# Check whether the function works
+top_n([8, 3, 2, 7, 4], 3)
+
